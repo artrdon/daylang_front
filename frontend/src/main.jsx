e550646f4ea3d7a0ecabdaf06cccd7953c65db0d
artrdon <sinmak.show@bk.ru>
2025-04-26 10:49:44 +0300

    beggining of using react query

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,4 +1,5 @@
 import { StrictMode, useState } from 'react'
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import Find from '/src/pages/find.jsx'
@@ -30,42 +31,47 @@ import Finded_deep from './pages/finded_deep.jsx'
 import pers from '/src/static/docks/pryvacy.pdf'
 import CallbackHandler from './pages/yandex_call_back.jsx'
 
+const queryClient = new QueryClient();
+
 createRoot(document.querySelector('body')).render(
-    <StrictMode>
-        <BrowserRouter>
+    
+        <StrictMode>
+            <QueryClientProvider client={queryClient}>
+                <BrowserRouter>
 
-            <Routes>
-                <Route path="/auth/yandex/callback" element={<CallbackHandler />} />
+                    <Routes>
+                        <Route path="/auth/yandex/callback" element={<CallbackHandler />} />
 
-                <Route path="/" element={<Find />}/>
-                <Route path="/user/undefined/" element={<Log />}/>
-                <Route path="/log/" element={<Log />}/>
-                <Route path="/log/reset/" element={<Log_reset />}/>
-                <Route path="/reg/" element={<Reg />}/>
-                <Route path="/create_offer/" element={<CreateOffer />}/>
-                <Route path="/update_offer/:username/:index/" element={<UpdateOffer />}/>
-                <Route path="/create_chat/:username/:id/" element={<ChatCreate />}/>
-                <Route path="/finded/:language/" element={<Finded />}/>
-                <Route path="/finded/:language/:format/:target/:age/:microphone/:min/:max/" element={<Finded_deep />}/>
-                <Route path="/t/user/:user/" element={<Me />}/>
-                <Route path="/t/user/:user/degree/" element={<Degree />}/>
-                <Route path="/t/user/:user/feedback/" element={<Feedback />}/>
-                <Route path="/p/user/:user/" element={<Me_pup />}/>
-                <Route path="/p/user/:user/feedback/" element={<Feedback_pup />}/>
-                <Route path="/t/user/:user/offers/" element={<Offers_on_main />}/>
-                <Route path="/message_list/" element={<Message_list />}/>
-                <Route path="/saved/" element={<Saved />}/>
-                <Route path="/message_list/:id/" element={<Message />}/>
-                <Route path="/settings/" element={<Settings />}/>
-                <Route path="/about_us/" element={<About />}/>
-                <Route path="/:username/offer/:id/" element={<Offer />}/>
-                <Route path='/my_lessons/' element={<MyLessons />} />
-                <Route path="/call/:id/" element={<Call />}/>
-                <Route path="*" element={<NotFound />} />
-            </Routes>
+                        <Route path="/" element={<Find />}/>
+                        <Route path="/user/undefined/" element={<Log />}/>
+                        <Route path="/log/" element={<Log />}/>
+                        <Route path="/log/reset/" element={<Log_reset />}/>
+                        <Route path="/reg/" element={<Reg />}/>
+                        <Route path="/create_offer/" element={<CreateOffer />}/>
+                        <Route path="/update_offer/:username/:index/" element={<UpdateOffer />}/>
+                        <Route path="/create_chat/:username/:id/" element={<ChatCreate />}/>
+                        <Route path="/finded/:language/" element={<Finded />}/>
+                        <Route path="/finded/:language/:format/:target/:age/:microphone/:min/:max/" element={<Finded_deep />}/>
+                        <Route path="/t/user/:user/" element={<Me />}/>
+                        <Route path="/t/user/:user/degree/" element={<Degree />}/>
+                        <Route path="/t/user/:user/feedback/" element={<Feedback />}/>
+                        <Route path="/p/user/:user/" element={<Me_pup />}/>
+                        <Route path="/p/user/:user/feedback/" element={<Feedback_pup />}/>
+                        <Route path="/t/user/:user/offers/" element={<Offers_on_main />}/>
+                        <Route path="/message_list/" element={<Message_list />}/>
+                        <Route path="/saved/" element={<Saved />}/>
+                        <Route path="/message_list/:id/" element={<Message />}/>
+                        <Route path="/settings/" element={<Settings />}/>
+                        <Route path="/about_us/" element={<About />}/>
+                        <Route path="/:username/offer/:id/" element={<Offer />}/>
+                        <Route path='/my_lessons/' element={<MyLessons />} />
+                        <Route path="/call/:id/" element={<Call />}/>
+                        <Route path="*" element={<NotFound />} />
+                    </Routes>
 
-        </BrowserRouter>
-    </StrictMode>,
+                </BrowserRouter>
+            </QueryClientProvider>
+        </StrictMode>,
 
 
 )
