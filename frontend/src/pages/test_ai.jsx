209dbf46c8ef53794df10260961031cf966878c1
artrdon <sinmak.show@bk.ru>
2025-05-28 13:31:02 +0300

    ai can commnicate with nymanoid voice, and spech to text is working good

--- frontend/src/pages/test_ai.jsx
+++ frontend/src/pages/test_ai.jsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import LoadingSpinner from '../elems/loading_spinner.jsx';
-import { useState, useEffect } from 'react'
+import { useState, useEffect, useRef } from 'react'
 import axios from 'axios';
 import APIURL from '/api.js'
 
 function TestAI() {
 
-    const [utterance, setUtterance] = useState(new SpeechSynthesisUtterance());
+    const audio = useRef(null);
+    const mediaRecorderRef = useRef(null);
+    const audioChunksRef = useRef([]);
+    const [isRecording, setIsRecording] = useState(false);
     const [speech, setSpeech] = useState(new webkitSpeechRecognition());
     const [isSpeaking, setIsSpeaking] = useState(false);
     const [voices, setVoices] = useState([]);
@@ -44,101 +47,80 @@ function TestAI() {
     
     }, []);
 
- /*   const speak = (text) => {
-        if(window.speechSynthesis) {
-            console.log("govory");
-            
-            utterance.text = text;
-            utterance.lang = "en";           // аббревиатура языка
-            utterance.volume = 8;          // громкость
-            utterance.rate = 1.1;            // скорость
-            utterance.pitch = 1;           // высота
-
-            // Обработчики событий
-            utterance.onstart = () => {
-                setIsSpeaking(true);
-                setSpeechStatus('Speaking...');
-            };
-        
-            utterance.onend = () => {
-                setIsSpeaking(false);
-                setSpeechStatus('Finished');
-                setTimeout(() => setSpeechStatus('Ready'), 2000);
-            };
-        
-            utterance.onerror = (event) => {
-                console.error('SpeechSynthesis error:', event.error);
-                setIsSpeaking(false);
-                setSpeechStatus(`Error: ${event.error}`);
-            };
-        
-            utterance.onpause = () => {
-                //setIsSpeaking(false);
-                setSpeechStatus('Paused');
-            };
-        
-            utterance.onresume = () => {
-                //setIsSpeaking(true);
-                setSpeechStatus('Resumed');
-            };
-        
-            utterance.onboundary = (event) => {
-             //   console.log('Reached boundary:', event);
-            };
-  
-            setIsSpeaking(true);
-            window.speechSynthesis.speak(utterance);
-        } 
-        else{
-            console.log("Feature not supported");
+    const speak = (text) => {
+        if (audio.current) {
+            audio.current.pause();
+            audio.current = null;
         }
+
+        const audioSrc = `data:${text['format']};base64,${text['audio']}`;
+        audio.current = new Audio(audioSrc);
+        audio.current.play();
+        setIsSpeaking(true);
+
+        audio.current.onended = () => {
+            console.log("Воспроизведение завершено");
+            audio.current = null;
+            setIsSpeaking(false);
+        };
     }
     const pause = () => {
-        window.speechSynthesis.pause(utterance);
-    }
+        if (audio.current && !audio.current.paused) {
+            audio.current.pause();
+        }
+    };
+
     const resume = () => {
-        window.speechSynthesis.resume(utterance);
-    }
+        if (audio.current && audio.current.paused) {
+            audio.current.play().catch(e => console.error("Resume failed:", e));
+        }
+    };
+
     const cancel = () => {
-        window.speechSynthesis.cancel(utterance);
-        setIsSpeaking(false);
-        setSpeechStatus('Stopped');
-    }
-    */
-    const send_request = async (e = null) => {
-        if (e) e.preventDefault();
+        if (audio.current) {
+            audio.current.pause();
+            audio.current.currentTime = 0; // Перемотка в начало
+            audio.current = null;
+            setIsSpeaking(false);
+            setSpeechStatus('Stopped');
+        }
         
+    };
+    const send_request = async (blob) => {
+      //  if (e) e.preventDefault();
+        
+        const formData = new FormData();
+        formData.append('audio', blob, 'recording.wav');
+        formData.append('answers', JSON.stringify(answers));
+
         try {
             setWaitForAnswer(true);
-            const response = await axios.post(`${APIURL}/airequest/`, {answers: answers, promt: promt}, {
+            const response = await axios.post(`${APIURL}/airequest/`, formData, {
                 headers: {
-                    'Content-Type': 'application/json',
+                    'Content-Type': 'multipart/form-data',
                     'X-CSRFToken': getCookie('csrftoken'),
                 },
             });
             console.log('Response:', response.data);    
             const newAnswer = {
-                myPromt: promt,
+                myPromt: response.data['myText'],
                 AIAnswer: response.data['text'],
             };
             setAnswers((answers) => [...answers, newAnswer]);
             
             if (typeof response.data === 'string') {
-                const audioSrc = `data:${response.data['format']};base64,${response.data['audio']}`;
-                const audio = new Audio(audioSrc);
-                audio.play();
+                speak(response.data)
+                
                 setPromt({promt: ""});
                 setWaitForAnswer(false);
             } else if (response.data.text) {
-                const audioSrc = `data:${response.data['format']};base64,${response.data['audio']}`;
-                const audio = new Audio(audioSrc);
-                audio.play();
+                speak(response.data)
+
                 setPromt({promt: ""});
                 setWaitForAnswer(false);
             } else {
-                const audioSrc = `data:${response.data['format']};base64,${response.data['audio']}`;
-                const audio = new Audio(audioSrc);
-                audio.play();
+                speak(response.data)
+                
                 setPromt({promt: ""});
                 setWaitForAnswer(false);
             }
@@ -148,73 +130,112 @@ function TestAI() {
         }
     };
 
-    const text_sintes = async (e = null) => {
-        if (e) e.preventDefault();
-        
-        try {
-            const response = await axios.post(`${APIURL}/aistt/`, {}, {
-                headers: {
-                    'Content-Type': 'application/json',
-                    'X-CSRFToken': getCookie('csrftoken'),
-                },
-            });
-            console.log('Response:', response.data); 
-            console.log(response.data['audio']);   
-            try {
-                const audioSrc = `data:${response.data['format']};base64,${response.data['audio']}`;
-                const audio = new Audio(audioSrc);
-                audio.play();
-            } catch (error) {
-                console.error("Ошибка декодирования аудио:", error);
-                // Fallback на текст или альтернативный плеер
+        // Функция для конвертации AudioBuffer в WAV
+        const audioBufferToWav = async (buffer) => {
+            const numOfChan = buffer.numberOfChannels;
+            const length = buffer.length * numOfChan * 2;
+            const buffer2 = new ArrayBuffer(44 + length);
+            const view = new DataView(buffer2);
+            const channels = [];
+            let offset = 0;
+            let pos = 0;
+    
+            // write WAVE header
+            setUint32(0x46464952);                         // "RIFF"
+            setUint32(36 + length);                        // file length - 8
+            setUint32(0x45564157);                         // "WAVE"
+            setUint32(0x20746d66);                         // "fmt " chunk
+            setUint32(16);                                 // length = 16
+            setUint16(1);                                  // PCM (uncompressed)
+            setUint16(numOfChan);
+            setUint32(buffer.sampleRate);
+            setUint32(buffer.sampleRate * 2 * numOfChan);  // avg. bytes/sec
+            setUint16(numOfChan * 2);                      // block-align
+            setUint16(16);                                 // 16-bit
+            setUint32(0x61746164);                         // "data" - chunk
+            setUint32(length);                             // chunk length
+    
+            // write interleaved data
+            for(let i = 0; i < buffer.numberOfChannels; i++) {
+                channels.push(buffer.getChannelData(i));
             }
-            
-        } catch (error) {
-            console.error('There was an error!', error.request.data);
+    
+            while(pos < buffer.length) {
+                for(let i = 0; i < numOfChan; i++) {
+                    let sample = Math.max(-1, Math.min(1, channels[i][pos]));
+                    sample = (0.5 + sample < 0 ? sample * 32768 : sample * 32767)|0;
+                    view.setInt16(44 + offset, sample, true);
+                    offset += 2;
+                }
+                pos++;
+            }
+    
+            function setUint16(data) {
+                view.setUint16(pos, data, true);
+                pos += 2;
+            }
+    
+            function setUint32(data) {
+                view.setUint32(pos, data, true);
+                pos += 4;
+            }
+    
+            return new Blob([buffer2], { type: 'audio/wav' });
         }
+    
+
+    const startRecording = async () => {
+        const stream = await navigator.mediaDevices.getUserMedia({ audio: true });
+        mediaRecorderRef.current = new MediaRecorder(stream, {
+            mimeType: 'audio/webm' // явно указываем формат записи
+        });
+        
+        mediaRecorderRef.current.ondataavailable = (e) => {
+            audioChunksRef.current.push(e.data);
+        };
+        
+        mediaRecorderRef.current.onstop = async () => {
+            // Сначала создаем blob в формате webm
+            const webmBlob = new Blob(audioChunksRef.current, { type: 'audio/webm' });
+            
+            // Конвертируем webm в wav
+            const audioContext = new AudioContext();
+            const audioData = await webmBlob.arrayBuffer();
+            const audioBuffer = await audioContext.decodeAudioData(audioData);
+            
+            // Создаем WAV файл
+            const wavBlob = await audioBufferToWav(audioBuffer);
+            
+            /*const audioUrl = URL.createObjectURL(wavBlob);
+            const audio = new Audio(audioUrl);
+            audio.play();
+            console.log("igra");*/
+            await send_request(wavBlob);
+            audioChunksRef.current = [];
+        };
+        
+        mediaRecorderRef.current.start();
+        setIsRecording(true);
     };
 
-    const startRecognition = () =>{
-        console.log("started recognition");
-        if(window.webkitSpeechRecognition) {
-            recognition.continuous = true;
-            recognition.lang = "en";    // распознавание речи на русском языке
-            recognition.start();    // начинаем распознавание
-        } else {
-            console.log("Распознавание речи НЕ поддерживается");
-        }
-    }
-    const endRecognition = () =>{
-        console.log("ended recognition");
-        recognition.stop();
-        index = 0;
-    }
 
-    recognition.onresult = function(event){ 
-        const results = event.results;  //   получаем список результатов
-        const firstResult = results[index++]; // получаем распознанный результат
-        const firstAlternative = firstResult[0];    // получаем первый вариант распознавания
-        const transcript = firstAlternative.transcript;  //  распознанный текст
-        const confidence = firstAlternative.confidence;    // уровень уверенности 
-        console.log(transcript);  
-        setPromt(transcript);
-        send_request();
-        console.log(confidence);
-    }
+    const stopRecording = () => {
+        mediaRecorderRef.current.stop();
+        setIsRecording(false);
+    };
 
     return (
         <>
 <div>
     {!waitForAnswer && <> 
         {!isSpeaking && <> 
-        <textarea name="promt" id="" value={promt.promt} onChange={handleInput} style={{position: "fixed", bottom: 0, left: 400, width: 500, height: 100, color: "black", backgroundColor: "white", resize: "none"}}></textarea>
-        <button onClick={send_request} style={{width: 200, height: 100, backgroundColor: "white", color: "black"}}>send_request</button> 
-        <button onClick={startRecognition} style={{width: 200, height: 100, backgroundColor: "white", color: "black"}}>start recogntion</button>
-        <button onClick={endRecognition} style={{width: 200, height: 100, backgroundColor: "white", color: "black"}}>end recognition</button> </>}
-        {/*<button onClick={pause} style={{width: 200, height: 100, backgroundColor: "white", color: "black"}}>pause</button>
+        {/*<textarea name="promt" id="" value={promt.promt} onChange={handleInput} style={{position: "fixed", bottom: 0, left: 400, width: 500, height: 100, color: "black", backgroundColor: "white", resize: "none"}}></textarea>
+        <button onClick={send_request} style={{width: 200, height: 100, backgroundColor: "white", color: "black"}}>send_request</button>*/} 
+        {!isRecording && <button onClick={startRecording} style={{width: 200, height: 100, backgroundColor: "white", color: "black"}}>start recording</button>}
+        {isRecording && <button onClick={stopRecording} style={{width: 200, height: 100, backgroundColor: "white", color: "black"}}>end recording</button>} </>}
+        <button onClick={pause} style={{width: 200, height: 100, backgroundColor: "white", color: "black"}}>pause</button>
         <button onClick={resume} style={{width: 200, height: 100, backgroundColor: "white", color: "black"}}>resume</button>
-        <button onClick={cancel} style={{width: 200, height: 100, backgroundColor: "white", color: "black"}}>cancel</button>*/}
-        <button onClick={text_sintes} style={{width: 200, height: 100, backgroundColor: "white", color: "black"}}>text synteth</button>
+        <button onClick={cancel} style={{width: 200, height: 100, backgroundColor: "white", color: "black"}}>cancel</button>
     </>}
     {waitForAnswer && <LoadingSpinner/>}
 </div>
